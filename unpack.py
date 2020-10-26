1446c566d4a9d6ab1d45a65c705012f8b1959ba9
Julian Maczuga <julian.maczuga@udo.edu>
2020-10-26 17:10:30 +0100

    Improvements

--- unpack.py
+++ unpack.py
@@ -1,44 +1,48 @@
 import struct
-   
+
+
 class VehicleModKit:
-    mods = [];
-    modNumTotal = 0;
-    exists = False;
-    modKitName = ""
-    
+
     def __init__(self, f):
-        modKitIndex = f.read(2);
-        modKitNameByteLength = f.read(2);
-        if len(modKitNameByteLength) > 0:
+        self.mods = []
+        self.modNumTotal = 0;
+        self.exists = False;
+        self.modKitName = "";
+
+        self.modKitIndex = f.read(2)  # Index of ModKit
+        modkit_length_as_bytes = f.read(2)
+
+        if len(modkit_length_as_bytes) > 0:
             self.exists = True
-            modKitNameLength = struct.unpack_from("<h", modKitNameByteLength)[0]
-            self.modKitName = str(f.read(modKitNameLength)).split('b\'', 1)[1].split('\'')[0] # C-Styles String Workaround
-            modNumByte = f.read(1);
-            self.modNumTotal = struct.unpack_from("<b", modNumByte)[0]
+            modkit_name_string_length = struct.unpack_from("<h", modkit_length_as_bytes)[0]
+
+            self.modKitName = str(f.read(modkit_name_string_length)).split('b\'', 1)[1].split('\'')[
+                0]  # C-Styles String Workaround
+
+
+            self.modNumTotal = struct.unpack_from("<b", f.read(1))[0]
             for x in range(self.modNumTotal):
-            
-                modType = struct.unpack_from("<b", f.read(1))[0]
-                modNum = struct.unpack_from("<b", f.read(1))[0]
-                
+
+                mod_type = struct.unpack_from("<b", f.read(1))[0]
+                total_mods_of_type = struct.unpack_from("<b", f.read(1))[0]
+
                 engineIndex = []
-                for mods in range(modNum):
+                for mods in range(total_mods_of_type):
                     engineIndex.append(struct.unpack_from("<h", f.read(2))[0])
-                
-                self.mods.append(Mod(modType, modNum, engineIndex))
-       
+
+                self.mods.append(Mod(mod_type, total_mods_of_type, engineIndex))
+
+
 class Mod:
-    modType = 0;
-    modNum = 0;
-    modIndex = [];
-    
+
     def __init__(self, t, n, m):
         self.modType = t
         self.modNum = n
         self.modIndex = m
-        
-        
-f = open("dirtymods.bin", "rb")
-#origOffset = 65517
+
+
+f = open("vehmods.bin", "rb")
+# origOffset = 65517
 dirtyOffset = 63040
 devoffset = 65517
 rcoffset = 65337
@@ -46,17 +50,16 @@ rcoffset = 65337
 try:
     magicByte = f.read(2)
     version = f.read(2)
-    
-    
-    #f.read(dirtyOffset)
+
+    # f.read(dirtyOffset)
     test = VehicleModKit(f);
-    while(test.exists):
-        print(test.modKitName + ": " +str(test.modNumTotal))
+    while (test.exists):
+        print(test.modKitName + ": " + str(test.modNumTotal))
         for x in range(len(test.mods)):
-           print("Mod: " + str(test.mods[x].modType) + " TotalMods: " + str(test.mods[x].modNum))
-        
+            print("Mod: " + str(test.mods[x].modType) + " TotalMods: " + str(test.mods[x].modNum))
+
         test = VehicleModKit(f)
         print("-------------------------------------------------------------------------------------")
-   
+
 finally:
     f.close()
